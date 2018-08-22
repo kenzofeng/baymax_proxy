f60d474c118e5b891f0d06cd00773c9a89396c93
daniel.liu <daniel.liu@derbysofot.com>
2018-08-22 17:03:06 +0800

    new init

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
+mkdir -p project/log
+mkdir -p project/report
+mkdir -p project/test_automation
+mkdir -p project/tmp
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
