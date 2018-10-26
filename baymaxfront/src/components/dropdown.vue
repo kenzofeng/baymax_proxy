a125ee9d215f753d2d5e34e01e01136d39a7cea0
daniel.liu <daniel.liu@derbysofot.com>
2018-10-26 14:44:07 +0800

    update

---
+++ baymaxfront/src/components/dropdown.vue
@@ -0,0 +1,44 @@
+<template>
+    <select class="ui search dropdown" v-model="selected">
+        <option value=""></option>
+        <option v-for="item in items" :value="item.title" :key="item.title"><i :class="iconcss(item)"></i>{{item.title}}</option>
+    </select >
+</template>
+<script>
+export default {
+  name: 'drop',
+  data () {
+    return {
+      selected: []
+    }
+  },
+  props: {items: null, default: null},
+  mounted () {
+    $(this.$el).dropdown({
+      clearable: true
+    })
+  },
+  watch: {
+    default: {
+      handler (newval, oldval) {
+        let _dropdwon = this
+        $(_dropdwon.$el).dropdown('clear')
+        if (newval !== undefined) {
+          newval.forEach(function (val) {
+            $(_dropdwon.$el).dropdown('set selected', val)
+          })
+          _dropdwon.selected = newval
+        }
+      }
+    },
+    selected (val) {
+      this.$emit('changeselected', val)
+    }
+  },
+  methods: {
+    iconcss (item) {
+      return item.icon + ' server icon'
+    }
+  }
+}
+</script>
