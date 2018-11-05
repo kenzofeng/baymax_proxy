56251866d4067cef5163369c75e19b536f243896
daniel.liu <daniel.liu@derbysofot.com>
2018-11-05 16:11:43 +0800

    update front move search to menu

--- robot_engine/utility.py
+++ robot_engine/utility.py
@@ -33,6 +33,7 @@ def stop_job(host):
     ssh.connect(host, 22, upload, upload_pwd, timeout=10.0)
     stdin, stdout, stderr = ssh.exec_command("ps -ef|grep 'python -m' |awk '{print $2}'|xargs sudo kill -9")
     stdin, stdout, stderr = ssh.exec_command("ps -ef|grep 'java -jar' |awk '{print $2}'|xargs sudo kill -9")
+    stdin, stdout, stderr = ssh.exec_command("sudo supervisorctl restart baymax")
     ssh.close()
     return "scuess"
 
