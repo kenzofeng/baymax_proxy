1fad434bd3eaadaeff92430840d5f61ec52a8d8d
daniel.liu <daniel.liu@derbysofot.com>
2018-12-10 17:24:50 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ cd baymaxfront
 npm run build
 cd  ..
 rm -rf baymaxproxy.tar.gz
-tar -zcvf  baymaxproxy.tar.gz ./Baymax_Proxy/*.py ./baymaxfront/dist/ ./robot_engine/*.py
+tar -zcvf  baymaxproxy.tar.gz ./Baymax_Proxy/*.py ./baymaxfront/dist/ ./robot_engine/*.py ./Pipfile ./Pipfile.lock ./manage.py
