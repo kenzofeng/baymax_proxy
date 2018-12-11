c24a8c66a60ba4dccc772a31a17bc410bc2ec785
daniel.liu <daniel.liu@derbysofot.com>
2018-12-11 13:54:16 +0800

    fix base64 in python3

--- Baymax_Proxy/settings.py
+++ Baymax_Proxy/settings.py
@@ -132,7 +132,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+            'level': 'ERROR',
         },
     },
 }
