7c7d169f6b216f4fba9fb38f19472dea0b033530
daniel.liu <daniel.liu@derbysofot.com>
2018-12-28 18:56:40 +0800

    add node admin function

--- proxy/admin.py
+++ proxy/admin.py
@@ -9,9 +9,10 @@ class SvnAdmin(admin.ModelAdmin):
 
 class NodeAdmin(admin.ModelAdmin):
     list_display = ['name', 'host', 'status']
+    actions = ['Set_Status_Done']
 
-    def Set_Status_Donw(self, request, queryset):
-        queryset.update(status='Donw')
+    def Set_Status_Done(self, request, queryset):
+        queryset.update(status='Done')
 
 
 class ProjectAdmin(admin.ModelAdmin):
