e3d265457dab6b0e7908c21d8ad8e94050a10fbd
daniel.liu <daniel.liu@derbysofot.com>
2018-12-28 10:20:54 +0800

    fix cat version bug

--- robot_engine/utility.py
+++ robot_engine/utility.py
@@ -48,7 +48,7 @@ def cat_version(host, version_paths):
     result_str = ""
     paths = version_paths.split(";")
     for path in paths:
-        result_str = path + '\n'
+        result_str += (path + '\n')
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         ssh.connect(host, 22, upload, upload_pwd, timeout=10.0)
