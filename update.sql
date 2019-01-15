2420a7bbde78aa0fc601550bea482faa1da386bb
daniel.liu <daniel.liu@derbysofot.com>
2019-01-15 16:30:44 +0800

    update sql

--- update.sql
+++ update.sql
@@ -6,3 +6,4 @@ alter table proxy_job_test add column end_time datetime;
 alter table proxy_job_test add column source_url varchar;
 alter table proxy_job_test add column source_type varchar;
 alter table proxy_job_test add column source_branch varchar;
+alter table proxy_job_test add column duration varchar;
