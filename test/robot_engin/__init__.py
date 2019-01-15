00bd9be8c63f04dd1278db0f4e464c90cf4a626e
daniel.liu <daniel.liu@derbysofot.com>
2019-01-15 13:40:34 +0800

    fix robot parse arguments

--- test/robot_engin/__init__.py
+++ test/robot_engin/__init__.py
@@ -1,6 +1,6 @@
 from robot_engine.utility import cat_version
 
 host = '127.0.0.1'
-path = '172.27.0.148:/usr/local/webapps/bestwestern-adapter/WEB-INF/classes/git.properties'
+path = '34.220.114.159:/usr/local/webapps/bestwestern-adapter/WEB-INF/classes/git.properties'
 
-cat_version(host, path)
+print (cat_version(host, path))
