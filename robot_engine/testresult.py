542df1963f37daea0dfb27f383f134332ff7c008
kenzo <416169126@qq.com>
2019-02-20 11:15:20 +0800

    fix write

--- robot_engine/testresult.py
+++ robot_engine/testresult.py
@@ -6,14 +6,14 @@ from robot.api import ExecutionResult, ResultWriter
 settings = namedtuple('Settings', ('output', 'xunit', 'report', 'log'))
 
 
-class MyExecutionResult(ExecutionResult):
+class MyResultWrite(ResultWriter):
 
     def _write(self, name, writer, path, *args):
         pass
 
 
 def merge_report(test_report_path, *report):
-    result = MyExecutionResult(*report, merge=True)
-    writer = ResultWriter(result)
+    result = ExecutionResult(*report, merge=True)
+    writer = MyResultWrite(result)
     writer.write_results(log=os.path.join(test_report_path, 'log'), output=os.path.join(test_report_path, 'output'),
                          report=os.path.join(test_report_path, 'report'))
