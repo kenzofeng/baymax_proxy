9cd9290897f79ca82ac7a6796f36e8ad2d5cdc01
kenzo <416169126@qq.com>
2019-02-20 11:00:35 +0800

    _write none

--- robot_engine/testresult.py
+++ robot_engine/testresult.py
@@ -1,10 +1,19 @@
 import os
+from collections import namedtuple
 
 from robot.api import ExecutionResult, ResultWriter
 
+settings = namedtuple('Settings', ('output', 'xunit', 'report', 'log'))
+
+
+class MyExecutionResult(ExecutionResult):
+
+    def _write(self, name, writer, path, *args):
+        pass
+
 
 def merge_report(test_report_path, *report):
-    result = ExecutionResult(*report, merge=True)
+    result = MyExecutionResult(*report, merge=True)
     writer = ResultWriter(result)
     writer.write_results(log=os.path.join(test_report_path, 'log'), output=os.path.join(test_report_path, 'output'),
                          report=os.path.join(test_report_path, 'report'))
