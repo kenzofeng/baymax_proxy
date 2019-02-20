e1865a735785704fbdd8b1261ff4f45b37e0ffd6
kenzo <416169126@qq.com>
2019-02-20 12:05:54 +0800

    fix write

--- robot_engine/testresult.py
+++ robot_engine/testresult.py
@@ -2,6 +2,9 @@ import os
 from collections import namedtuple
 
 from robot.api import ExecutionResult, ResultWriter
+from robot.errors import DataError
+from robot.output import LOGGER
+from robot.utils import unic
 
 settings = namedtuple('Settings', ('output', 'xunit', 'report', 'log'))
 
@@ -9,7 +12,18 @@ settings = namedtuple('Settings', ('output', 'xunit', 'report', 'log'))
 class MyResultWrite(ResultWriter):
 
     def _write(self, name, writer, path, *args):
-        pass
+        try:
+            writer(path, *args)
+        except DataError as err:
+            LOGGER.error(err.message)
+        except EnvironmentError as err:
+            # `err.filename` can be different than `path` at least if reading
+            # log/report templates or writing split log fails.
+            # `unic` is needed due to http://bugs.jython.org/issue1825.
+            LOGGER.error("Writing %s file '%s' failed: %s: %s" %
+                         (name.lower(), path, err.strerror, unic(err.filename)))
+        else:
+            pass
 
 
 def merge_report(test_report_path, *report):
